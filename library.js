4d3b18c97eda0e5d8824ccf12d8ef61bbd83f235
Rinzin Dolma <rsamrab7@gmail.com>
2025-06-11 14:55:01 -0400

    Added get and post methods along with middleware functions

---
+++ library.js
@@ -0,0 +1,12 @@
+export let library = [
+  {
+    title: "I love Mom",
+    author: "Rinzin Dolma",
+    datePublished: 2025,
+  },
+  {
+    title: "My name is Lula",
+    author: "Ngawang Samrab",
+    datePublished: 2025,
+  },
+];
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-const express = require("express");
+import express from "express";
+import { library } from "./library.js";
+
+// const express = require("express");
 const app = express();
 const port = 3000;
+const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
 app.listen(port, () => {
-  console.log("Server is running")
+  console.log("server is running");
 });
+
+app.use(express.json());
+// middleware function
+app.use("/library", router);
+
+router
+  .route("/books") //this library will acccept get and post methods
+  .get((req, res) => {
+    res.send(library);
+  })
+  .post((req, res) => {
+    const book = req.body;
+    console.log(book);
+    res.status(201).send("Created successfully");
+  });
+
+// `<label for="bookTitle">Book Title:</label>
+//     <input type="text" placeholder="Name of the book" id="title"/>
+// <label for="authorName"> Name of Author: </label>
+//     <input type="text" placeholder="Author" id = "author"/>
+// <label for ="date">Published Date: </label>
+//     <input type="date" placeholder="Published Date" id="datePublished"/>
+// <button type = "submit">Submit</button>`
+
+// http://localhost:3000/library/books
