326bd1df0e17dd4416f81d2a3eb033e48492d6cb
Rinzin Dolma <rsamrab7@gmail.com>
2025-06-16 04:03:26 -0400

    tied the loose ends

--- server.js
+++ server.js
@@ -1,89 +1,120 @@
 import express from "express";
 
+import methodOverride from "method-override";
+
 import { library } from "./library.js";
 
 const app = express();
 const port = 3000;
 
 //register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req,res)=>{
-res.render('index', {library});
-});
-
-app.listen(port, () => {
-  console.log("Server is running");
-});
-
-// middleware function
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//Methods
+app.get("/", (req, res) => {
+  res.render("index", { library });
+});
+
+app.get("/library/books", (req, res) => {
+  res.send(library);
+});
+
+app.post("/library/books", (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    return res.status(400).send("Title and author are required");
+  }
+  const newBook = { id: library.length + 1, title, author };
+  library.push(newBook);
+
+  if (req.headers["content-type"]==="application/json"){
+    return res.status(201).send(newBook);
+  }
+
+  res.redirect("/");
+
+});
+
+app.patch("/library/books/update", (req, res, next) => {
+  const { id } = req.body;
+  if (!id) {
+    res.status(400).send("ID number is a  required field");
+  }
+  req.url = `/library/books/${id}`;
+  next();
+});
+
+app.patch("/library/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { author } = req.body;
+
+  const book = library.find((b) => b.id === id);
+  if (!book) {
+   return res.status(404).send("Unsuccessful. Book not found");
+  }
+  book.author = author;
+
+  if(req.headers["content-type"]==="application/json"){
+    return res.status(200).send(book);
+  }
+  res.redirect("/");
+});
+
+// app.patch("/library/books/:id", (req, res) => {
+//   const id = req.params.id;
+//   const updatedAccount = req.body;
+//   const index = accounts.findIndex((account) => account.id === parseInt(id));
+//   if (index !== -1) {
+//     accounts[index] = { ...accounts[index], ...updatedAccount };
+//     res.send(accounts[index]);
+//   } else {
+//     res.status(404).send({ error: "Account not found" });
+//   }
+// });
+// app.put("library/books/:id", (req, res) => {
+//   const book = library.find((item) => item.id == req.params.id);
+//   if (book) {
+//     const { title, author, datePublished } = req.body;
+//     book.title = title;
+//     book.author = author;
+//     book.datePublished = datePublished;
+//     res.send(book);
+//   } else {
+//     res.status(404).send("Unsuccessful");
+//   }
+//   res.send("Data received successfully");
+//   res.status(201).send("Update successful");
+// });
+app.delete("/library/books/delete", (req, res, next) => {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).send("ID is a required field");
+  }
+  req.url = `/library/books/${id}`;
+  next();
+});
+app.delete("/library/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = library.findIndex((book) => book.id === id);
+
+if (index === -1) {
+ return res.status(404).send("Not found");
+}
+library.splice(index, 1);
+res.redirect("/");
+});
+
+//middleware error functions
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong");
 });
 
-
-// // router
-// //   .route("/books") //this library will acccept get and post methods
-
-app.get("/library/books", (req, res, next) => {
-  res.send(library);
+app.listen(port, () => {
+  console.log("Server is running");
 });
-app.post("/library/books", (req, res) => {
-  const { title, author, datePublished } = req.body;
-  const book = {
-    id: library.length + 1,
-    title,
-    author,
-    datePublished,
-  };
-  library.push(book);
-  res.status(201).send("Created successfully");
-});
-
- app.patch('/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedAccount = req.body;
-    const index = accounts.findIndex(account => account.id === parseInt(id));
-    if (index !== -1) {
-      accounts[index] = { ...accounts[index], ...updatedAccount };
-      res.send(accounts[index]);
-    } else {
-      res.status(404).send({ error: 'Account not found' });
-    }
-  })
-app.put("library/books/:id", (req, res) => {
-  const book = library.find((item) => item.id == req.params.id);
-  if (book) {
-    const { title, author, datePublished } = req.body;
-    book.title = title;
-    book.author = author;
-    book.datePublished = datePublished;
-    res.send(book);
-  } else {
-    res.status(404).send("Unsuccessful");
-  }
-    res.send("Data received successfully");
-    res.status(201).send("Update successful");
-});
-app.delete("/library/books", (req, res) => {
-  const book = req.body;
-  library.pop(book);
-  res.send(library);
-  res.status(201).send("Delete successful");
-});
-
-// .delete((req,res)=>{
-//   res.
-// });
-
-// `<label for="bookTitle">Book Title:</label>
-//     <input type="text" placeholder="Name of the book" id="title"/>
-// <label for="authorName"> Name of Author: </label>
-//     <input type="text" placeholder="Author" id = "author"/>
-// <label for ="date">Published Date: </label>
-//     <input type="date" placeholder="Published Date" id="datePublished"/>
-// <button type = "submit">Submit</button>`
-
-// http://localhost:3000/library/books
